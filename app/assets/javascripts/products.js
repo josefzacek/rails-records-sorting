fa180a6a90a4b3266540fcf2f676e4b88c9d502f
Josef Zacek <info@josefzacek.com>
2016-05-09 00:23:49 +0100

    add handle to list

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,6 +1,9 @@
 jQuery( document ).ready(function( $ ) {
   $('#products-listing').sortable({
     axis: "y",
+    handle: function() {
+      ".drag-handle"
+    },
     update: function() {
       $.post($(this).data('update-url'), $(this).sortable('serialize'))
     }
